63bb4a9646e1b9e882fd1a0bbfa28bff42c511fa
tony148565 <51195315+tony148565@users.noreply.github.com>
2020-05-16 13:16:47 +0800

    version2

--- req_get.py
+++ req_get.py
@@ -1,6 +1,13 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+
 d = {'key1': 'value1', 'key2': 'value2'}
 man_url = ''
 
@@ -10,3 +17,11 @@ def get_html(url, url2):
     r = requests.post(url, data=json.dumps(d), headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
     return soup
+
+
+def get_sel_html(url):
+    driver_path = r"C:\Chrome_driver\chromedriver.exe"
+    driver = webdriver.Chrome(driver_path, chrome_options=chrome_options)
+    cc = driver.page_source
+    driver.close()
+    return cc
