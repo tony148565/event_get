5ab6ec89d8fa28756d1f5f66615f5ff47ff2c54b
tony148565 <51195315+tony148565@users.noreply.github.com>
2020-05-29 19:53:55 +0800

    version 2

--- Main.py
+++ Main.py
@@ -3,7 +3,9 @@ import __init__
 import accupass
 import datetime
 import time
-import SQL_connect
+import String_process
+import locate
+#import SQL_connect
 
 
 while 1:
@@ -11,7 +13,13 @@ while 1:
     date = " "
     date = date + str(timenow.year) + "/" + str(timenow.month) + "/" + str(timenow.day)
     print(date)
-    __init__.taichanggov(date)
+    #__init__.taichanggov(date)
     accupass.accupassget()
+    #gg = locate.loc('卓越商務中心')
+    #print(gg.get('latitude'))
+    #print(gg.get('longitude'))
+    #re = String_process.date_and_time("2020-06-13T14:00:00")
+    #gg = String_process.address_where("台灣台中市403西區臺灣大道二段285號31樓3117教室")
+    #print(gg)
     print("zzzzzzzzzz")
     time.sleep(3)
