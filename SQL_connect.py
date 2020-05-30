7fea756e6e978b02a3b3c25d41f559f4fc9a6cfd
tony148565 <51195315+tony148565@users.noreply.github.com>
2020-05-30 12:42:45 +0800

    version 4

--- SQL_connect.py
+++ SQL_connect.py
@@ -49,19 +49,66 @@ def select(table):
     cursor = conn.cursor()
     cursor.execute(query)
     result = cursor.fetchall()
-    print(result)
+    #print(type(result))
+    #print(result)
+    return result
+
 
 
 # id目前的使用狀況，要用前記得+=1
 
 
-def insertdata():
-    print("run!!")
-    idflag = {"activityID": 3, "bookingID": 0, "addressID": 0}
-    idflag["activityID"] += 1
-    dic = {"activityID": idflag["activityID"], "name": "測試", "activityURL": "https://...", "startDate": "2020-01-01",
-           "endDate": "2020-01-01"}
+def insertdata(push_main, push_add, push_book):
+    #print("run!!")
+    #idflag = {"activityID": 3, "bookingID": 0, "addressID": 0}
+    #idflag["activityID"] += 1
+    push_main['activityID'] = check_main(push_main)
+    push_add['addressID'] = check_add(push_add)
+    push_main['addressID'] = push_add.get('addressID')
+    push_book['bookingID'] = check_booking(push_book)
+    push_main['bookingID'] = push_book.get('bookingID')
+    if final_check('ActivityMain', push_main.get('activityID')):
+        insertIntoDatabase("ActivityMain", push_main)
+    if final_check('Address', push_main.get('addressID')):
+        insertIntoDatabase("ActivityMain", push_main)
+    if final_check('Booking', push_main.get('bookingID')):
+        insertIntoDatabase("ActivityMain", push_main)
+    #dic = {"activityID": idflag["activityID"], "name": "測試", "activityURL": "https://...", "startDate": "2020-01-01", "endDate": "2020-01-01"}
     #insertIntoDatabase("ActivityMain", dic)
-    select("ActivityMain")
+    #select("ActivityMain")
     # 確認執行
     conn.commit()
+
+
+def check_main(push):
+    listd = select("ActivityMain")
+    print(len(listd))
+    for i in listd:
+        if i[1] == push.get('name'):
+            return i[0]
+    return len(listd)+1
+
+
+def check_add(push):
+    lista = select("Address")
+    print(len(lista))
+    for i in lista:
+        if i[1] == push.get('eastLongitude') and i[2] == push.get('northLatitude'):
+            return i[0]
+    return len(lista)+1
+
+
+def check_booking(push):
+    listb = select("Booking")
+    for i in listb:
+        if i[1] == push.get('bookingURL'):
+            return i[0]
+    return len(listb)+1
+
+
+def final_check(table, uid):
+    listd = select(table)
+    if uid < len(listd):
+        return False
+    else:
+        return True
