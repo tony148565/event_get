14af7c408a605ead6e53905a5d2e9a59b5a91b94
tony148565 <51195315+tony148565@users.noreply.github.com>
2020-06-02 15:20:40 +0800

    version 5

--- SQL_connect.py
+++ SQL_connect.py
@@ -52,63 +52,72 @@ def select(table):
     #print(type(result))
     #print(result)
     return result
-
-
-
 # id目前的使用狀況，要用前記得+=1
 
 
 def insertdata(push_main, push_add, push_book):
-    #print("run!!")
-    #idflag = {"activityID": 3, "bookingID": 0, "addressID": 0}
-    #idflag["activityID"] += 1
     push_main['activityID'] = check_main(push_main)
     push_add['addressID'] = check_add(push_add)
     push_main['addressID'] = push_add.get('addressID')
     push_book['bookingID'] = check_booking(push_book)
     push_main['bookingID'] = push_book.get('bookingID')
-    if final_check('ActivityMain', push_main.get('activityID')):
-        insertIntoDatabase("ActivityMain", push_main)
+
     if final_check('Address', push_main.get('addressID')):
-        insertIntoDatabase("ActivityMain", push_main)
+        push_add['addressID'] = str(check_add(push_add))
+        insertIntoDatabase("Address", push_add)
     if final_check('Booking', push_main.get('bookingID')):
+        push_book['bookingID'] = str(check_booking(push_book))
+        insertIntoDatabase("Booking", push_book)
+    if final_check('ActivityMain', push_main.get('activityID')):
+        push_main['activityID'] = str(push_main.get('activityID'))
+        push_main['addressID'] = str(push_add.get('addressID'))
+        push_main['bookingID'] = str(push_book.get('bookingID'))
         insertIntoDatabase("ActivityMain", push_main)
-    #dic = {"activityID": idflag["activityID"], "name": "測試", "activityURL": "https://...", "startDate": "2020-01-01", "endDate": "2020-01-01"}
-    #insertIntoDatabase("ActivityMain", dic)
-    #select("ActivityMain")
+
     # 確認執行
     conn.commit()
 
 
 def check_main(push):
     listd = select("ActivityMain")
-    print(len(listd))
+    #print("ActivityMain")
+    #print(len(listd))
+    #print(listd)
     for i in listd:
         if i[1] == push.get('name'):
+            print("same event")
             return i[0]
     return len(listd)+1
 
 
 def check_add(push):
     lista = select("Address")
-    print(len(lista))
+    #print("Address")
+    #print(len(lista))
     for i in lista:
-        if i[1] == push.get('eastLongitude') and i[2] == push.get('northLatitude'):
+        if i[5] == push.get('fullAddress'):
+            print("same address")
             return i[0]
     return len(lista)+1
 
 
 def check_booking(push):
     listb = select("Booking")
+    #print("Booking")
+    #print(len(listb))
     for i in listb:
         if i[1] == push.get('bookingURL'):
+            print("same url")
             return i[0]
     return len(listb)+1
 
 
 def final_check(table, uid):
     listd = select(table)
-    if uid < len(listd):
+    print(type(uid))
+    print(uid)
+    uuid = int(uid)
+    if uuid <= len(listd):
         return False
     else:
         return True
