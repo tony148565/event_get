98abfeac2da59d8d696b93ef39f5196df7e8c8a7
tony148565 <51195315+tony148565@users.noreply.github.com>
2020-06-13 15:27:10 +0800

    version 4

--- String_process.py
+++ String_process.py
@@ -1,10 +1,54 @@
 
 
-def date_and_time(dateandtime):
+def date_and_time(dateandtime):  # for accupass
     time = dateandtime.rpartition('T')
     #print(time)
     re = {'date': time[0], 'time': time[2]}
     #print(re)
+    return re
+
+
+def kktix_date(time_list):  # ex: 2000/01/01 --> 2000-01-01
+    afrp = []  # after replace
+    for i in time_list:
+        rpart = i.rpartition('(')
+        tt = rpart[0]
+        rpart2 = tt.rpartition('(')
+        tt2 = rpart2[0]
+        tt3 = rpart2[2]
+        rpart3 = tt3.rpartition(' ')
+        tt4 = rpart3[2]
+        tt5 = tt4 + ":00"
+        afrp.append(tt2.replace('/', '-'))
+        afrp.append(tt5)
+    if len(afrp) == 2:
+        afrp.append(afrp[0])
+        afrp.append(afrp[1])
+    re = {'startDate': afrp[0], 'openTime': afrp[1], 'endTime': afrp[2], 'closeTime': afrp[3]}
+    return re
+
+
+def kktix_booking_date(time_list):
+    afrp = []
+    for i in time_list:
+        rpart = i.rpartition('(')
+        tt = rpart[0]
+        tt2 = tt.replace('/', '-')
+        tt3 = tt2 + ':00'
+        afrp.append(tt3)
+    if len(afrp) == 1:
+        afrp.append(afrp[0])
+    #  print(afrp)
+    return afrp
+
+
+def kktix_react(strr):
+    #  print(strr)
+    strr = strr.rpartition('<!--')
+    #  print(strr)
+    st = strr[0].rpartition('-->')
+    #  print(st[2])
+    re = st[2]
     return re
 
 
